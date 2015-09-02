1d42a7d3e9d29d8182fd7a183b7ef07fb500c53d
Zeke Y <ezekiel@yarashus.com>
2015-09-01 22:11:46 -0400

    Show # of unread notifications in the tab title

--- index.js
+++ index.js
@@ -9,15 +9,3 @@ pageMod.PageMod({
   include: "*.codecademy.com",
   contentScriptFile: [self.data.url("./jquery-1.11.1.min.js"), self.data.url("./add-count.js"), self.data.url("./forum-link.js"), self.data.url("./magic.js"), self.data.url("./profile-updater.js"), self.data.url("qa-updater.js")]
 });
-
-
-
-
-
-// a dummy function, to show how tests work.
-// to see how to test this function, look at test/test-index.js
-//function dummy(text, callback) {
-//  callback(text);
-//}
-//
-//exports.dummy = dummy;
