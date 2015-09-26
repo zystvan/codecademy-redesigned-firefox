33c401ae6fe5c2e3cd5fefb5dbed5ee6628ed07f
Zeke Y <zeke@zystvan.com>
2015-09-26 10:37:41 -0400

    Update the look of forum links & <ul>s to match the blog

--- data/forum-updater.js
+++ data/forum-updater.js
@@ -1,83 +1,75 @@
-function forumUpdater() {
-  // show the correct exercise location
-  if (unsafeWindow.CCDATA && unsafeWindow.CCDATA.forum != "undefined" && window.location.pathname.split("/")[window.location.pathname.split("/").length - 2] == "forum_questions") {
-    var id = window.location.pathname.split("/")[window.location.pathname.split("/").length - 1];
-    
-    $.getJSON(window.location, function(e) {
-      h = $("div#section #breadcrumb .breadcrumb b a").clone();
-      $("div#section #breadcrumb .breadcrumb b a").remove();
-      t = $("div#section #breadcrumb .breadcrumb b").text().split(" ");
-      t[1] = (parseInt(e.section_index) + 1).toString(10);
-      $("div#section #breadcrumb .breadcrumb b").html(t.slice(0, 4).join(" ") + " (" + (typeof e.humanized_exercise_index != "undefined" ? "Exercise " + e.humanized_exercise_index.toString(10) : "General Forum") + ")").prepend(h);
-    });
-  }
-  
-  // restyle and add some basic functionality back into the search form
-  $('#forum_search input[type="text"]').attr("autocomplete", "off");
-  $('#forum_search input[type="submit"]').attr("value", "Search");
-  $('.section').addClass("forum_question"); // so the user can search through sections of a course
-  $('.section_link').addClass("title"); // so the user can search through sections of a course
-  
-  if (window.location.search && window.location.search.substr(0, 3) == "?q=") {
-    searchVal = window.location.search.substr(3);
-    var input = $('#forum_search input[type="text"]'),
-        posts = document.querySelectorAll('.forum_question'),
-        postTitle = document.querySelectorAll('.title');
+// show the correct exercise location
+if (unsafeWindow.CCDATA && unsafeWindow.CCDATA.forum != "undefined" && window.location.pathname.split("/")[window.location.pathname.split("/").length - 2] == "forum_questions") {
+  var id = window.location.pathname.split("/")[window.location.pathname.split("/").length - 1];
 
-    input.val(searchVal);
-
-    for (var i = 0; i < posts.length; i++) {
-      if (postTitle[i].textContent.toLowerCase().indexOf(searchVal) > -1) {
-        posts[i].style['display'] = 'inline-block';
-      }
-      else {
-        posts[i].style['display'] = 'none';
-      }
-    }
-  }
-  
-  $('#forum_search input[type="text"]').keydown(function(event) {    
-    var input = $('#forum_search input[type="text"]'),
-        searchVal = input.val().toLowerCase(),
-        posts = document.querySelectorAll('.forum_question'),
-        postTitle = document.querySelectorAll('.title');
-    
-    if (input.val() == "" && event.which == 13) {
-      event.stopPropagation();
-      event.preventDefault();
-    }
-    
-    for (var i = 0; i < posts.length; i++) {
-      if (!searchVal || postTitle[i].textContent.toLowerCase().indexOf(searchVal) > -1) {
-        posts[i].style['display'] = 'inline-block';
-      }
-      else {
-        posts[i].style['display'] = 'none';
-      }
-    }
-  });
-  
-  // link to specific comments/responses
-//  $("body").append("<style>.timestamp:hover, div.answer_comments_block div.comment-details span:hover { cursor: pointer; color: #F65A5B;}</style>");
-
-  $(document).on('click', '.timestamp', function() {
-//	var url = 'https://www.codecademy.com' + window.location.pathname + '#'; 
-    link = $(this).closest('li.forum_response').attr('id');
-    
-    if (link == undefined) {
-      link = "";
-    }
-    
-//    window.location = url + link;
-    window.location = window.location + "#" + link;
-  }); 
-
-  $(document).on('click', 'div.answer_comments_block div.comment-details span', function() {
-    var url = 'https://www.codecademy.com' + window.location.pathname + '#'; 
-    link = $(this).closest('li.forum_response_comment').attr('id'); 
-    
-    window.location = url + link;
+  $.getJSON(window.location, function(e) {
+    h = $("div#section #breadcrumb .breadcrumb b a").clone();
+    $("div#section #breadcrumb .breadcrumb b a").remove();
+    t = $("div#section #breadcrumb .breadcrumb b").text().split(" ");
+    t[1] = (parseInt(e.section_index) + 1).toString(10);
+    $("div#section #breadcrumb .breadcrumb b").html(t.slice(0, 4).join(" ") + " (" + (typeof e.humanized_exercise_index != "undefined" ? "Exercise " + e.humanized_exercise_index.toString(10) : "General Forum") + ")").prepend(h);
   });
 }
 
-forumUpdater();
+// restyle and add some basic functionality back into the search form
+$('#forum_search input[type="text"]').attr("autocomplete", "off");
+$('#forum_search input[type="submit"]').attr("value", "Search");
+$('.section-nav li.section').addClass("forum_question qa"); // so the user can search through sections of a course
+$('.section_link').addClass("title"); // so the user can search through sections of a course
+
+if (window.location.search && window.location.search.substr(0, 3) == "?q=") {
+  var input = $('#forum_search input[type="text"]'),
+      searchVal = decodeURIComponent(window.location.search.substr(3)).replace(/\+/g, " "),
+      posts = $('.forum_question.qa');
+
+  input.val(searchVal);
+  
+  searchVal = searchVal.toLowerCase();
+
+  for (var i = 0; i < posts.length; i++) {
+    if (posts[i].textContent.toLowerCase().indexOf(searchVal) > -1) {
+      posts[i].style['display'] = 'inline-block';
+    }
+    else {
+      posts[i].style['display'] = 'none';
+    }
+  }
+}
+
+$('#forum_search input[type="text"]').keydown(function(event) {    
+  var input = $('#forum_search input[type="text"]'),
+      searchVal = input.val().toLowerCase(),
+      posts = $('.forum_question.qa');
+
+  if (input.val() == "" && event.which == 13) {
+    event.stopPropagation();
+    event.preventDefault();
+  }
+
+  for (var i = 0; i < posts.length; i++) {
+    if (!searchVal || posts[i].textContent.toLowerCase().indexOf(searchVal) > -1) {
+      posts[i].style['display'] = 'inline-block';
+    }
+    else {
+      posts[i].style['display'] = 'none';
+    }
+  }
+});
+
+$(document).on('click', '.timestamp', function() {
+  var url = window.location.protocol + "//" + window.location.host + window.location.pathname;
+  link = $(this).closest('li.forum_response').attr('id');
+
+  if (link == undefined) {
+    link = "";
+  }
+
+  window.location = url + "#" + link;
+}); 
+
+$(document).on('click', 'div.answer_comments_block div.comment-details span', function() {
+  var url = window.location.protocol + "//" + window.location.host + window.location.pathname;
+  link = $(this).closest('li.forum_response_comment').attr('id'); 
+
+  window.location = url + "#" + link;
+});
--- data/site-updater.js
+++ data/site-updater.js
@@ -10,14 +10,16 @@ if (unsafeWindow.CCDATA && unsafeWindow.CCDATA.current_user) {
     oldCount = count;
 
     $.getJSON(unreadNotificationsUrl, function(data) {
-      count = data.unread_count
+      count = data.unread_count;
     }).done(function() {
-      if (oldCount != count && count != undefined) {
+      if (count != oldCount && count != undefined) {
         if (count == 0) {
-            $("title").html(title);
+          $("title").html(title);
+//          $('.icon-bell').removeClass('.new-notification');
         }
         else {
-          $("title").html("(" + count + ") " + title);
+          $('title').html("(" + count + ") " + title);
+//          $('.icon-bell').addClass('.new-notification');
 
           if (window.Notification && Notification.permission != "denied") {
             Notification.requestPermission(function(status) {
@@ -40,8 +42,10 @@ if (unsafeWindow.CCDATA && unsafeWindow.CCDATA.current_user) {
       }
     });
   }
-  setTimeout(updateCount, 7000);
-  setInterval(updateCount, 7000);
+  
+  updateCount();
+  setTimeout(updateCount, 5000);
+  setInterval(updateCount, 5000);
 }
 
 // add extra data to user profiles
@@ -51,8 +55,8 @@ userDataUrl = window.location.protocol + "//" + userDataUrl;
 $.getJSON(userDataUrl, function(d) {
   unsafeWindow.CCDATA.user = d
 }).done(function() {
-   $(".profiles.show article.fit-full.color-scheme--darkgrey").after('<article class="fit-full color-scheme--darkgrey"><div class="fit-fixed"><div class="grid-row profile-time" style="text-align: center;"><div class="grid-col-4 grid-col--align-center" style="float: none; display: inline-block;"><h3 class="padding-right--quarter">' + unsafeWindow.CCDATA.user.points_today + '</h3><small>points today</small></div><div class="grid-col-4 grid-col--align-center" style="float: none; display: inline-block;"><h3 class="padding-right--quarter">' + unsafeWindow.CCDATA.user.streak_hash.max_count + '</h3><small>day best streak</small></div><div class="grid-col-4 grid-col--align-center" style="float: none; display: inline-block"><h3 class="padding-right--quarter">' + unsafeWindow.CCDATA.user.points_hash.best_points_day + '</h3><small>Best points day</small></div></div></div></article>');
+   $(".profiles.show article.fit-full.color-scheme--darkgrey").after('<article class="fit-full color-scheme--darkgrey"><div class="fit-fixed"><div class="grid-row profile-time" style="text-align: center;"><div class="grid-col-4 grid-col--align-center" style="float: none; display: inline-block;"><h3 class="padding-right--quarter">' + unsafeWindow.CCDATA.user.points_today + '</h3><small>points today</small></div><div class="grid-col-4 grid-col--align-center" style="float: none; display: inline-block;"><h3 class="padding-right--quarter">' + unsafeWindow.CCDATA.user.streak_hash.max_count + '</h3><small>day best streak</small></div><div class="grid-col-4 grid-col--align-center" style="float: none; display: inline-block"><h3 class="padding-right--quarter">' + unsafeWindow.CCDATA.user.points_hash.best_points_day + '</h3><small>best points day</small></div></div></div></article>');
 });
-  
+
 // add a link in the footer pointing to old group posts
 $("div#footer__main div#footer__company__links").after('<br><a href="https://github.com/A-J-C/CodecademyGroups">Codecademy Group Posts Saved on GitHub</a>');
