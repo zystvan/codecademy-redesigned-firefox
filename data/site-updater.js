4be1910ca689ae0567492cbce4a5ed99a2fb7349
Zeke Y <zeke@zystvan.com>
2015-10-06 19:31:52 -0400

    Wait longer between checks for unread notifications & go back to the old way of inserting the CC Groups link in the footer

--- data/site-updater.js
+++ data/site-updater.js
@@ -44,8 +44,8 @@ if (unsafeWindow.CCDATA && unsafeWindow.CCDATA.current_user) {
   }
 
   updateCount();
-  setTimeout(updateCount, 7000);
-  setInterval(updateCount, 7000);
+  setTimeout(updateCount, 90000);
+  setInterval(updateCount, 90000);
 }
 
 // add extra data to user profiles
@@ -58,9 +58,4 @@ $.getJSON(userDataUrl, function(d) {
    $(".profiles.show article.fit-full.color-scheme--darkgrey").after('<article class="fit-full color-scheme--darkgrey"><div class="fit-fixed"><div class="grid-row profile-time" style="text-align: center;"><div class="grid-col-4 grid-col--align-center" style="float: none; display: inline-block;"><h3 class="padding-right--quarter">' + unsafeWindow.CCDATA.user.points_today + '</h3><small>points today</small></div><div class="grid-col-4 grid-col--align-center" style="float: none; display: inline-block;"><h3 class="padding-right--quarter">' + unsafeWindow.CCDATA.user.streak_hash.max_count + '</h3><small>day best streak</small></div><div class="grid-col-4 grid-col--align-center" style="float: none; display: inline-block"><h3 class="padding-right--quarter">' + unsafeWindow.CCDATA.user.points_hash.best_points_day + '</h3><small>best points day</small></div></div></div></article>');
 });
 
-var brElement = document.createElement("br"),
-    aElement = document.createElement("a");
-aElement.setAttribute("href", "https://github.com/A-J-C/CodecademyGroups");
-aElement.textContent = "Codecademy Group posts saved on GitHub";
-document.querySelector('#footer #footer__main .grid-col-4').appendChild(brElement);
-document.querySelector('#footer #footer__main .grid-col-4').appendChild(aElement);
+$('#footer #footer__main .grid-col-4').append('<br><a href="https://github.com/A-J-C/CodecademyGroups">Codecademy Group posts saved on GitHub</a>');
